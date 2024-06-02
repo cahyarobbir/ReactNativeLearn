27962a0ba33c7a3af0211740b55562379a63965e
cahyarobbir <63894172+cahyarobbir@users.noreply.github.com>
2024-06-02 13:52:05 +0700

    Try text Component

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import TryTextComponent from './Component/text';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <Text style={styles.font}>Hello world</Text>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
-      </View>
-      <StatusBar style="dark" />
-    </SafeAreaView>
+    <TryTextComponent />
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    paddingTop: StatusBar.cu,
-    marginHorizontal: 16,
-  },
-
-  container: {
-    backgroundColor: 'black',
-    height: 30,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  font: {
-    color: 'white',
-    fontSize: 12
-  }
-
-});
---
+++ Component/text.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TryTextComponent() {
+    return (
+        <View>
+            <Text>Test Text Component</Text>
+        </View>
+    );
+};
